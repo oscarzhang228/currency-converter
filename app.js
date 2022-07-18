b27b1d6f55c54a1c095c57a57b482d487d0ad1ae
Oscar Zhang <oscarzhang228@gmail.com>
2022-07-17 22:34:54 -0400

    get javascript working

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ var requestOptions = {
   headers: myHeaders,
 };
 
-// function getConversion(to, from, amount) {
-//   fetch(
-//     `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,
-//     requestOptions
-//   )
-//     .then((response) => response.json())
-//     .then((theResult) => console.log(theResult.result))
-//     .catch((error) => console.log("error", error));
-// }
+const result = document.getElementById("result");
+
+function getConversion(to, from, amount) {
+  fetch(
+    `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((theResult) => (result.innerText = theResult.result + " " + to))
+    .catch((error) => console.log("error", error));
+}
+
+const convert = document.getElementById("convert");
+document.getElementById("convert").addEventListener("click", () => {
+  getConversion(
+    document.getElementById("to").value,
+    document.getElementById("from").value,
+    document.querySelector("input").value
+  );
+});
